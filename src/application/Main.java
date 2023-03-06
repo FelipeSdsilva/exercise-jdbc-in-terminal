85080e04df4f8ef06453505029729163e6a01945
Felipe Sousa <felipe.fps09@hotmail.com>
2023-03-06 10:45:26 -0300

    Insert two values in one clausule

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,11 +3,7 @@ package application;
 import db.DB;
 import exceptions.DbException;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
+import java.sql.*;
 import java.time.format.DateTimeFormatter;
 
 public class Main {
@@ -20,21 +16,41 @@ public class Main {
 
         try {
             conn = DB.getConnection();
-
+ /*
             prepSt = conn.prepareStatement("INSERT INTO seller " +
                     "(name, email, birthdate, basesalary, departmentid) " +
                     "VALUES " +
-                    "(?,?,?,?,?)");
+                    "(?,?,?,?,?)", Statement.RETURN_GENERATED_KEYS);
 
             prepSt.setString(1, "Felipe Sousa");
             prepSt.setString(2, "felipesouls@gmail.com");
             prepSt.setTimestamp(3, Timestamp.valueOf(LocalDateTime.parse("09/05/1994 00:00", formatter)));
             prepSt.setDouble(4, 3000.00);
             prepSt.setInt(5, 4);
+*/
+            prepSt = conn.prepareStatement("INSERT INTO department (name) VALUES ('Food'),('Toys')",
+                    Statement.RETURN_GENERATED_KEYS);
 
             int rowsAffected = prepSt.executeUpdate();
 
-            System.out.println("Done! Row affected: " + rowsAffected);
+            if (rowsAffected > 0) {
+                ResultSet rs = prepSt.getGeneratedKeys();
+
+                while (rs.next()) {
+                    int id = rs.getInt(1);
+                    /*
+                    System.out.println("Done! id= " + id);
+                    System.out.println("Seller: { " +
+                            "\nname: '" + rs.getString("name") + "'," +
+                            "\nemail: " + rs.getString("email") + "," +
+                            "\nbirthdate: " + rs.getTimestamp("birthdate").toLocalDateTime().format(formatter) + "," +
+                            "\nbasesalary: " + rs.getDouble("basesalary") + "," +
+                            "\ndepartmentid: " + rs.getInt("departmentid") + "\n};");
+                      */
+                }
+            } else {
+                System.out.println("No rows affected!");
+            }
 
         } catch (SQLException e) {
             throw new DbException(e.getMessage());
