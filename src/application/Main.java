d361be18bb962640950a33343e79779db3ecc682
Felipe Sousa <felipe.fps09@hotmail.com>
2023-03-06 10:59:55 -0300

    Test update

--- src/application/Main.java
+++ src/application/Main.java
@@ -16,38 +16,18 @@ public class Main {
 
         try {
             conn = DB.getConnection();
- /*
-            prepSt = conn.prepareStatement("INSERT INTO seller " +
-                    "(name, email, birthdate, basesalary, departmentid) " +
-                    "VALUES " +
-                    "(?,?,?,?,?)", Statement.RETURN_GENERATED_KEYS);
 
-            prepSt.setString(1, "Felipe Sousa");
-            prepSt.setString(2, "felipesouls@gmail.com");
-            prepSt.setTimestamp(3, Timestamp.valueOf(LocalDateTime.parse("09/05/1994 00:00", formatter)));
-            prepSt.setDouble(4, 3000.00);
-            prepSt.setInt(5, 4);
-*/
-            prepSt = conn.prepareStatement("INSERT INTO department (name) VALUES ('Food'),('Toys')",
-                    Statement.RETURN_GENERATED_KEYS);
+            prepSt = conn.prepareStatement("UPDATE seller " +
+                    "SET basesalary = basesalary + ? " +
+                    "WHERE departmentid = ?");
+
+            prepSt.setDouble(1, 2000.00);
+            prepSt.setInt(2, 2);
 
             int rowsAffected = prepSt.executeUpdate();
 
             if (rowsAffected > 0) {
-                ResultSet rs = prepSt.getGeneratedKeys();
-
-                while (rs.next()) {
-                    int id = rs.getInt(1);
-                    /*
-                    System.out.println("Done! id= " + id);
-                    System.out.println("Seller: { " +
-                            "\nname: '" + rs.getString("name") + "'," +
-                            "\nemail: " + rs.getString("email") + "," +
-                            "\nbirthdate: " + rs.getTimestamp("birthdate").toLocalDateTime().format(formatter) + "," +
-                            "\nbasesalary: " + rs.getDouble("basesalary") + "," +
-                            "\ndepartmentid: " + rs.getInt("departmentid") + "\n};");
-                      */
-                }
+                System.out.println(rowsAffected);
             } else {
                 System.out.println("No rows affected!");
             }
